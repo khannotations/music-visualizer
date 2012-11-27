4ae90a8a54c88f6a8f0811fb3bad9a39d3f99269
Rafi Khan <faiaz.a.khan@gmail.com>
2012-11-27 00:34:42 -0500

    reformatted

--- server/src/ServiceThread.java
+++ server/src/ServiceThread.java
@@ -11,120 +11,89 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import org.json.simple.JSONValue;
 
 public class ServiceThread extends Thread {
-
-    private ServerSocket welcomeSocket;
-    private Map<String, String> updateTable;
-
-    private BufferedReader inFromClient;
-    private DataOutputStream outToClient;
-    
-    public ServiceThread(ServerSocket welcomeSocket,
-			 Map<String, String> updateTable) {
-	this.welcomeSocket = welcomeSocket;
-    }
-
-    public void run() {
-
-	System.out.println("Thread " + this + " started.");
-	while (true) {
+  private ServerSocket welcomeSocket;
+  private Map<String, String> updateTable;
+  private BufferedReader inFromClient;
+  private DataOutputStream outToClient;
+  public ServiceThread(ServerSocket welcomeSocket,
+  Map<String, String> updateTable) {
+    this.welcomeSocket = welcomeSocket;
+  }
+  public void run() {
+    System.out.println("Thread " + this + " started.");
+  	while (true) {
 	    // get a new request connection
 	    Socket s = null;
-	    
 	    synchronized (welcomeSocket) {
-		try {
-		    s = welcomeSocket.accept();
-		    System.out.println("Thread " + this + " process request "
-				       + s);
-		} catch (IOException e) {
-		}
-	    } // end of extract a request
-	    
-	    processRequest(s);
-
-	} // end while
-	
-    } // end run
-
-    // request
-    
-    //
-    private void processRequest(Socket connSock) {
-	
-	try {
+    		try {
+    		    s = welcomeSocket.accept();
+    		    System.out.println("Thread "+this+" process request "+s);
+    		} catch (IOException e) {
+    		}
+      } // end of extract a request
+      processRequest(s);
+  	} // end while
+  } // end run
+  private void processRequest(Socket connSock) {
+    try {
 	    // create read stream to get input
 	    inFromClient = new BufferedReader(new InputStreamReader(connSock.getInputStream()));
 	    outToClient = new DataOutputStream(connSock.getOutputStream());
-	    
 	    String query = inFromClient.readLine();
-            String[] request = query.split("\\s");
+      String[] request = query.split("\\s");
 	    if (request.length < 2 || !request[0].equals("GET")) {
-		outputError(500, "Bad request");
-		connSock.close();
-		return;
-	    }
-            
+		    outputError(500, "Bad request");
+		    connSock.close();
+		    return;
+	    }    
 	    //path
-	    String[] path = request[1].split("\\?");
-                
-            //http parameters
+	    String[] path = request[1].split("\\?");      
+      //http parameters
 	    String[] params = path[1].split("&");
 	    Map<String, String> map = new HashMap<String, String>();
 	    for (String param : params) {
-		String name = param.split("=")[0];
-		String value = param.split("=")[1];
-		map.put(name, value);
-	    }
-            
-            //Leaving this for reference on how to access http parameters
+        String name = param.split("=")[0];
+        String value = param.split("=")[1];
+        map.put(name, value);
+	    } 
+      //Leaving this for reference on how to access http parameters
 	    /*String me = map.get("me");
 	    String meV = map.get("meV");*/
-	    
 	    String action = path[0];
-            
-            if(action.equals("/new")) {
-                //TODO
-            } else if (action.equals("/join")) {
-                //TODO
-            } else if (action.equals("/touch")) {
-                //TODO
-            } else {
-                outputError(500, "Bad request");
-		connSock.close();
-		return;
-
-            }
-	    
-            String out = action; //TODO Fix this
+      if(action.equals("/new")) {
+          //TODO
+      } else if (action.equals("/join")) {
+          //TODO
+      } else if (action.equals("/touch")) {
+          //TODO
+      } else {
+        outputError(500, "Bad request");
+        connSock.close();
+        return;
+      }
+      String out = action; //TODO Fix this
 	    outputResponseHeader();
 	    outputResponseBody(out);
 	    connSock.close();
-	    
-	} catch (Exception e) {
-	}
-	
-    } // end of serveARequest
-
-
-    private void outputResponseHeader() throws Exception {
-	outToClient.writeBytes("HTTP/1.0 200 Document Follows\r\n");
+    } catch (Exception e) {
     }
-
-    private void outputResponseBody(String out) throws Exception {
-	outToClient.writeBytes("Content-Length: " + out.length() + "\r\n");
-	outToClient.writeBytes("\r\n");
-	outToClient.writeBytes(out);
-
-    }
-
-    void outputError(int errCode, String errMsg) {
-	try {
-	    outToClient.writeBytes("HTTP/1.0 " + errCode + " " + errMsg
-				   + "\r\n");
-	} catch (Exception e) {
-	}
-    }
+  } // end of serveARequest
+  private void outputResponseHeader() throws Exception {
+    outToClient.writeBytes("HTTP/1.0 200 Document Follows\r\n");
+  }
+  private void outputResponseBody(String out) throws Exception {
+  	outToClient.writeBytes("Content-Length: " + out.length() + "\r\n");
+  	outToClient.writeBytes("\r\n");
+  	outToClient.writeBytes(out);
+  }
+  void outputError(int errCode, String errMsg) {
+  	try {
+  	    outToClient.writeBytes("HTTP/1.0 " + errCode + " " + errMsg
+  				   + "\r\n");
+  	} catch (Exception e) {
+  	}
+  }
 } // end ServiceThread
