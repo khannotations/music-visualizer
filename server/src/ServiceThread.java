0741350c0ed7331051522cd0c859a089255ea775
Rafi Khan <faiaz.a.khan@gmail.com>
2012-11-28 23:55:08 -0500

    now an eclipse project

--- server/src/ServiceThread.java
+++ server/src/ServiceThread.java
@@ -1,10 +1,15 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * 
  */
-import java.io.*;
-import java.util.*;
-import java.net.*;
-import org.json.simple.JSONValue;
 
 public class ServiceThread extends Thread {
   private ServerSocket welcomeSocket;
--- server/src/TouchEvent.java
+++ server/src/TouchEvent.java
@@ -1,15 +1,6 @@
 /*
  * 
  */
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.*;
-import org.json.simple.JSONValue;
 
 public class TouchEvent {
   private float startX;
