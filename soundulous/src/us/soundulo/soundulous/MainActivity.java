c40811b7db3152116a40ba1bc12bf5bb46dc1cd2
jacobmetrick <omnigamer@gmail.com>
2012-11-28 22:18:12 -0500

    now has menu options for starting and joining a new game but they don't do anything

--- soundulous/src/us/soundulo/soundulous/MainActivity.java
+++ soundulous/src/us/soundulo/soundulous/MainActivity.java
@@ -1,6 +1,7 @@
 package us.soundulo.soundulous;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
@@ -8,6 +9,8 @@ import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
+	
+	private Handler handler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,10 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
+        	case R.id.start_session:
+        		return true;
+        	case R.id.join_session:
+        		return true;
             case R.id.menu_settings:
             	// launch menu settings
             	Intent launcherIntent = new Intent(this, SettingsActivity.class);
@@ -37,4 +44,5 @@ public class MainActivity extends Activity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    
 }
