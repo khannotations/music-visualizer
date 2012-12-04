087094b146789237694537ccfdc558f1552743d3
hariganesan <hari.ganesan@yale.edu>
2012-12-03 19:05:30 -0500

    Added new sample gestures for flings.

--- soundulous/src/us/soundulo/soundulous/TouchableView.java
+++ soundulous/src/us/soundulo/soundulous/TouchableView.java
@@ -1,31 +1,55 @@
 package us.soundulo.soundulous;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
-public class TouchableView extends View {
-
-	private class GestureListener extends SimpleOnGestureListener {
-		private TouchableView view;
-		
-		public GestureListener (TouchableView view) {
-			this.view = view;
-		}
-	}
-
-	
-	private GestureDetector gestures;
+public class TouchableView extends View {	
+	private GestureDetector gestureDetector;
+	View.OnTouchListener gestureListener;
+	Context context = this.context;
+	private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 	
 	public TouchableView(Context context) {
 	    super(context);
-	    gestures = new GestureDetector(getContext(),
-	            new GestureListener(this));
+
+
+	    gestureDetector = new GestureDetector(getContext(),
+	            new GestureListener());
+	    gestureListener = new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				return gestureDetector.onTouchEvent(event);
+			}
+		};
 	}
 	
-	public boolean onTouchEvent(MotionEvent event) {
-	    return gestures.onTouchEvent(event);
+//	public boolean onTouchEvent(MotionEvent event) {
+//	    return gestureDetector.onTouchEvent(event);
+//	}
+	// taken mostly from http://stackoverflow.com/questions/937313/android-basic-gesture-detection
+	private class GestureListener extends SimpleOnGestureListener {
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                // right to left swipe
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Toast.makeText(context, "Left Swipe", Toast.LENGTH_SHORT).show();
+                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Toast.makeText(context, "Right Swipe", Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) {
+                // nothing
+            }
+            return false;
+        }
 	}
 }
